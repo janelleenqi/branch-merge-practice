f8625aa97078eabc0e59527a2c98f0245d671c92
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-14 16:48:06 +0800

    print x10

--- Main.java
+++ Main.java
@@ -2,4 +2,7 @@ public class Main {
     int theNumber = 0;
     System.out.println("Help me");
     System.out.println(theNumber);
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+    }
 }
